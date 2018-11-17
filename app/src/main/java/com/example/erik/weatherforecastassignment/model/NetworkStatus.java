861eb8b59868cee83a0536e71526f00ee923a578
Erik Lindfors <elindfo@kth.se>
2018-11-17 14:00:30 +0100

    Fix bug where app would crash on no active network connection

--- app/src/main/java/com/example/erik/weatherforecastassignment/model/NetworkStatus.java
+++ app/src/main/java/com/example/erik/weatherforecastassignment/model/NetworkStatus.java
@@ -15,7 +15,10 @@ public class NetworkStatus {
             (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
     public static Status getStatus(){
-        if(connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork()).hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)){
+        if(connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork()) == null){
+            return Status.NO_CONNECTION;
+        }
+        else if(connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork()).hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)){
             return Status.MOBILE;
         }
         else if(connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork()).hasTransport(NetworkCapabilities.TRANSPORT_WIFI)){
