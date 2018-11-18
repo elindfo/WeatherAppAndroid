d6b6beaacc3609183ccd68c71edc2793edb5e8a0
Erik Lindfors <elindfo@kth.se>
2018-11-18 02:51:20 +0100

    Add landscape layout for main activity

--- app/src/main/java/com/example/erik/weatherforecastassignment/view/MainActivity.java
+++ app/src/main/java/com/example/erik/weatherforecastassignment/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.erik.weatherforecastassignment.view;
 
+import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -41,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT ? R.layout.activity_main_portrait : R.layout.activity_main_landscape);
 
         weatherModel = WeatherModel.getInstance();
 
